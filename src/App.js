8bf9d626b02da8cb610a7d42e1a169d56699f2b9
Mohamed Aashik Ali <aashiq22@gmail.com>
2025-01-12 19:42:19 +0530

    array length updated again

--- src/App.js
+++ src/App.js
@@ -41,8 +41,8 @@ const [items,setItems] = useState([
 
 ])
 
+const numbers = [-2,-1,2,3,4,5,6,7,8]
 
-const numbers = [-2,-1,2,3,4,5,6]
 const itemss = numbers.filter(n =>n>0).map((n) =>({numbers:n}))
 console.log(itemss)
 
